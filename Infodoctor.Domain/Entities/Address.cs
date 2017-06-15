5783c7720bc1ba26b948c082ce29865baa59bb78
Stanislav Reva <stunislaf.mail@gmail.com>
2017-06-15 22:29:15 +0300

    create file for address class

---
+++ Infodoctor.Domain/Entities/Address.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace Infodoctor.Domain.Entities
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public virtual ICollection<LocalizedAddress> LocalizedAddresses { get; set; }
+        public virtual ICollection<Phone> Phones { get; set; }
+        public virtual Clinic Clinic { get; set; }
+        public virtual ICollection<Doctor> Doctors { get; set; }
+    }
+
+    public class Phone
+    {
+        public int Id { get; set; }
+        public virtual ICollection<LocalizedPhone> LocalizedPhones { get; set; }
+        public virtual Address Address { get; set; }
+    }
+
+    public class LocalizedAddress
+    {
+        public int Id { get; set; }
+        public string Country { get; set; }
+        public virtual City City { get; set; }
+        public string Street { get; set; }
+        public virtual Language Language { get; set; }
+    }
+
+    public class LocalizedPhone
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Number { get; set; }
+        public virtual Language Language { get; set; }
+    }
+}
