4be247fcefba4ca307a16013be0d7df736181a30
sergeikd <sergeikd@gmail.com>
2017-06-30 19:25:03 +0300

    Add static web-site strings service

---
+++ Infodoctor.Web/App_Start/StaticStringsProvider.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Web;
+using Microsoft.VisualBasic.FileIO;
+
+namespace Infodoctor.Web
+{
+    public static class StaticStringsProvider
+    {
+        public static string PathToStaticStrings => ConfigurationManager.AppSettings["PathToStaticStrings"];
+        public static string SupportedLanguages => ConfigurationManager.AppSettings["LangCodes"];
+
+        public static void PrepareStaticStrings()
+        {
+            StaticStrings.LanguageNames = SupportedLanguages.Split(',').ToArray();
+            StaticStrings.StringValues = ReadFile(StaticStrings.LanguageNames.Length);
+        }
+
+        public static List<string[]> ReadFile(int languagesQuantity)
+        {
+            var pathToCsvFile = HttpContext.Current.Server.MapPath("~") + PathToStaticStrings;
+            var staticStringValues = new List<string[]>();
+            using (var parser = new TextFieldParser(pathToCsvFile, Encoding.GetEncoding(1251)))
+            {
+                parser.TextFieldType = Microsoft.VisualBasic.FileIO.FieldType.Delimited;
+                parser.SetDelimiters(new string[] {","});
+                while (!parser.EndOfData)
+                {
+                    string[] fields = parser.ReadFields();
+                    var readLanguagesLine = new string[languagesQuantity + 1];
+                    for (var i = 0; i <= languagesQuantity; i++)
+                    {
+                        readLanguagesLine[i] = fields[i];
+                    }
+                    staticStringValues.Add(readLanguagesLine);
+                }
+            }
+            return staticStringValues;
+        }
+    }
+
+    public static class StaticStrings
+    {
+        public static string[] LanguageNames { get; set; }
+        public static List<string[]> StringValues { get; set; }
+    }
+}
---
+++ Infodoctor.Web/Controllers/StaticStringsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Web.Http;
+using Infodoctor.Web.Infrastructure;
+using Newtonsoft.Json;
+
+namespace Infodoctor.Web.Controllers
+{
+    public class StaticStringsController : ApiController
+    {
+        private readonly ConfigService _configService;
+        public StaticStringsController(ConfigService configService)
+        {
+            if (configService == null)
+                throw new ArgumentNullException(nameof(configService));
+            _configService = configService;
+        }
+
+        // GET: api/{lang}/StaticStrings
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("api/{lang}/StaticStrings")]
+        public string Get(string lang)
+        {
+            var language = StaticStrings.LanguageNames.Contains(lang) ? lang : _configService.DefaultLangCode;
+            var index = Array.IndexOf(StaticStrings.LanguageNames, language);
+            var result = StaticStrings.StringValues.ToDictionary(line => line[0], line => line[index + 1]);
+            var json = JsonConvert.SerializeObject(result, Formatting.Indented);
+            return json;
+        }
+    }
+}
